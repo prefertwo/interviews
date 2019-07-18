fdb8b268214157e0251ce45a9032d1b4a8f8eeb9
guozheng <1522876658@qq.com>
2019-07-18 18:23:34 +0800

    对象引用

--- js/console.js
+++ js/console.js
@@ -45,4 +45,5 @@ changeObjProperty(webSite);
 console.log(webSite.siteUrl);
 /**
  * 函数传参：函数的形参是值得传递，如果形参是对象的话，函数接受的是这个对象的指针地址。而 new Object() 声明了一个新对象，和原对象没关系了。原对象的值不会再有变化。
+ * 对象作为参数，传递进去的是这个对象的引用地址，o.siteUrl 是给这个对象赋值，o = new Object() 是把 o 指向另一个对象，o.siteUrl 是给这个新对象赋值，不影响 webSite 这个变量指向的那个对象，两个 o 指向的对象的引用地址不同；
  */
