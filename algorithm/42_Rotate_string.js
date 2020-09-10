b928ba99730ce576b34efdee8fae35ca936f804d
prefertwo <1522876658@qq.com>
2020-09-10 12:28:13 +0800

    lc

--- algorithm/42_Rotate_string.js
+++ algorithm/42_Rotate_string.js
@@ -6,6 +6,20 @@
  * 输出：“student. a am I”
  */
 
+var reverseWords = function (s) {
+  // 需要的操作，去掉两端空格、分割、反转、合并
+  return s.split(' ').filter(item => item).reverse().join(' ');
+  // 因为用到了 filter 所以可以去掉前面的 trim
+};
+// 执行用时：76 ms, 在所有 JavaScript 提交中击败了72.54%的用户
+// 内存消耗：39.1 MB, 在所有 JavaScript 提交中击败了33.63%的用户
+
+const testArr = ['the sun is hot', 'a good   example', "  hello world!  "];
+// 期望输出：[ 'hot is sun the', 'example good a', "world! hello" ]
+const returnArr = testArr.map(item => reverseWords(item))
+console.log(returnArr);
+
+
 /**
  * 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。
  * 定义一个函数实现字符串的左旋转操作的功能。
